6de1b1f467b321c9032e7b07b8e40e1718b8d03d
Carlos Zuluaga Mora <carloszulmor@unisabana.edu.co>
2023-12-16 10:15:08 -0500

    Crea el contenedor y el componente Boton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './Componentes/Boton';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>¡Hola, Mundo!</h1>
+      <div className="Calculadora">
+        <div className="Fila">
+          <Boton></Boton>
+        </div>
+        <div className="Fila"></div>
+        <div className="Fila"></div>
+        <div className="Fila"></div>
+        <div className="Fila"></div>
+      </div>
     </div>
   );
 }
---
+++ src/Componentes/Boton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import "../Stylesheets/Boton.css";
+
+function Boton(props) {
+  return (
+    <div>
+      {props.children}
+    </div>
+  );
+}
+
+export default Boton;
