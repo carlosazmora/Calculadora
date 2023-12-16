1d9521685ee4a2b2be3ae82a330d3ff58da17e14
Carlos Zuluaga Mora <carloszulmor@unisabana.edu.co>
2023-12-16 10:40:49 -0500

    Añade botones y componente Pantalla

--- src/App.js
+++ src/App.js
@@ -7,13 +7,32 @@ function App() {
       <h1>¡Hola, Mundo!</h1>
       <div className="Calculadora">
         <div className="Fila">
-          <Boton>1</Boton>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
           <Boton>+</Boton>
         </div>
-        <div className="Fila"></div>
-        <div className="Fila"></div>
-        <div className="Fila"></div>
-        <div className="Fila"></div>
+        <div className="Fila">
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className="Fila">
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className="Fila">
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>=</Boton>
+          <Boton>/</Boton>
+        </div>
+        <div className="Fila">
+          
+        </div>
       </div>
     </div>
   );
